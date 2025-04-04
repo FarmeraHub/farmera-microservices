b941f017c647f59891c5906251ac2543913201b4
21120442 <duydangtr@gmail.com>
2025-04-04 22:21:39 +0700

    chore(setup): initialize project with actix-web setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_files::NamedFile;
+use actix_web::{web::resource, App, HttpServer, Responder};
+use dotenvy::dotenv;
+use env_logger::Env;
+
+async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let server_addr = env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS must be set");
+    let server_port = env::var("SERVER_PORT").expect("SERVER_PORT must be set");
+
+    HttpServer::new(|| App::new().service(resource("/").to(index)))
+        .bind(format!("{server_addr}:{server_port}"))?
+        .workers(1)
+        .run()
+        .await
 }
