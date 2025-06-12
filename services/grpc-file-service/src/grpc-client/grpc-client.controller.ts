7926e3b325c84d362f285d41d43debb21b8f523a
21120442 <duydangtr@gmail.com>
2025-06-12 14:17:07 +0700

    feat: upload multiple files

--- services/grpc-file-service/src/grpc-client/grpc-client.controller.ts
+++ services/grpc-file-service/src/grpc-client/grpc-client.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { GrpcClientService } from './grpc-client.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('grpc-client')
 export class GrpcClientController {
@@ -11,5 +11,11 @@ export class GrpcClientController {
     @UseInterceptors(FileInterceptor('file'))
     async upload(@UploadedFile() file: Express.Multer.File) {
         return await this.service.upload(file);
+    }
+
+    @Post("multi-files")
+    @UseInterceptors(FilesInterceptor('files'))
+    async multipleUpload(@UploadedFiles() files: Express.Multer.File[]) {
+        return await this.service.multipleUpload(files);
     }
 }
--- services/grpc-file-service/src/grpc-client/grpc-client.service.ts
+++ services/grpc-file-service/src/grpc-client/grpc-client.service.ts
@@ -1,6 +1,7 @@
 import { FileServiceClient, UploadImageRequest, UploadImageResponse } from '@farmera/grpc-proto/dist/files/files';
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
+import { randomUUID } from 'crypto';
 import { firstValueFrom, Observable, Subject } from 'rxjs';
 
 @Injectable()
@@ -26,6 +27,7 @@ export class GrpcClientService implements OnModuleInit {
                     file_name: file.originalname,
                     file_type: file.mimetype,
                     total_size: file.buffer.length,
+                    file_id: randomUUID(),
                 },
             });
 
@@ -48,5 +50,46 @@ export class GrpcClientService implements OnModuleInit {
         });
 
         return firstValueFrom(this.grpcFileService.uploadImage(request$));
+    }
+
+    async multipleUpload(files: Express.Multer.File[]): Promise<UploadImageResponse[]> {
+        const CHUNK_SIZE = 64 * 1024; // 64 KB
+
+        const uploadPromises = files.map(file => {
+            const request$ = new Observable<UploadImageRequest>(subscriber => {
+                // send metadata
+                subscriber.next({
+                    meta: {
+                        file_name: file.originalname,
+                        file_type: file.mimetype,
+                        total_size: file.buffer.length,
+                        file_id: randomUUID(),
+                    },
+                });
+
+                console.log("buffer total size: ", file.buffer.length);
+
+                // send chunks
+                let offset = 0;
+                while (offset < file.buffer.length) {
+                    const end = Math.min(offset + CHUNK_SIZE, file.buffer.length);
+                    const chunk = file.buffer.subarray(offset, end);
+
+                    subscriber.next({ image: chunk });
+
+                    offset = end;
+
+                    console.log("sent: ", offset);
+                }
+
+                // close stream
+                subscriber.complete();
+            });
+
+            return firstValueFrom(this.grpcFileService.uploadImage(request$));
+        });
+
+        // Đợi tất cả upload hoàn thành
+        return Promise.all(uploadPromises);
     }
 }
