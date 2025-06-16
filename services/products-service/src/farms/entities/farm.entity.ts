2b5cc9bb6a300864bebb3e884a870620f10e9970
Nguyen Tuan Anh <ntanh.fit@gmail.com>
2025-06-16 17:48:13 +0700

    chore: add UUID generation for entities and update package dependencies

--- services/products-service/src/farms/entities/farm.entity.ts
+++ services/products-service/src/farms/entities/farm.entity.ts
@@ -1,13 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToOne, UpdateDateColumn, OneToMany } from "typeorm";
-import { Address } from "./address.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToOne,
+  UpdateDateColumn,
+  OneToMany,
+  PrimaryColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { Address } from './address.entity';
 import { FarmStatus } from '../../common/enums/farm-status.enum';
-import { Product } from "../../products/entities/product.entity";
-import { Identification } from "./identification.entity";
+import { Product } from '../../products/entities/product.entity';
+import { Identification } from './identification.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity()
 export class Farm {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn('uuid')
   farm_id: string;
+
+  @BeforeInsert()
+  generateId() {
+    if (!this.farm_id) {
+      this.farm_id = uuidv4();
+    }
+  }
 
   @Column()
   farm_name: string;
@@ -15,7 +33,7 @@ export class Farm {
   @Column()
   description: string;
 
-  @Column({nullable: true})
+  @Column({ nullable: true })
   avatar_url: string;
 
   @Column('text', { array: true, nullable: true })
@@ -33,10 +51,10 @@ export class Farm {
   @Column()
   tax_number: string;
 
-  @Column({ 
-    type: 'enum', 
-    enum: FarmStatus, 
-    default: FarmStatus.PENDING 
+  @Column({
+    type: 'enum',
+    enum: FarmStatus,
+    default: FarmStatus.PENDING,
   })
   status: FarmStatus;
 
@@ -48,12 +66,14 @@ export class Farm {
 
   @OneToOne(() => Address, (address) => address.farm, { cascade: true })
   address: Address;
-  @OneToOne(()=> Identification,(identification) => identification.farm, {cascade:true} )
+  @OneToOne(() => Identification, (identification) => identification.farm, {
+    cascade: true,
+  })
   identification: Identification;
 
   @Column('uuid')
   user_id: string;
 
-  @OneToMany(()  => Product, (product) => product.farm, { cascade: true })
+  @OneToMany(() => Product, (product) => product.farm, { cascade: true })
   products: Product[];
 }
