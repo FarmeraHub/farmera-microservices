938b52ad20146811cddb906b5322064d494c3e74
21120442 <duydangtr@gmail.com>
2025-07-14 10:19:20 +0700

    fix: ssl config

--- services/payment-service/src/config/typeorm.config.ts
+++ services/payment-service/src/config/typeorm.config.ts
@@ -1,10 +1,10 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from "@nestjs/typeorm";
 
-export const typeOrmAsyncConfig:TypeOrmModuleAsyncOptions ={
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
     imports: [ConfigModule],
     inject: [ConfigService],
-    useFactory:(configService:ConfigService)=>({
+    useFactory: (configService: ConfigService) => ({
         type: configService.get<any>('DB_TYPE'),
         host: configService.get<string>('PG_HOST'),
         port: parseInt(configService.get<string>('PG_PORT') || '5432'),
@@ -12,7 +12,14 @@ export const typeOrmAsyncConfig:TypeOrmModuleAsyncOptions ={
         password: configService.get<string>('PG_PASSWORD'),
         database: configService.get<string>('PG_DB'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: configService.get<boolean>('DB_SYNC',true),
-        logging: configService.get<boolean>('DB_LOGGING',false),
+        synchronize: configService.get<boolean>('DB_SYNC', true),
+        logging: configService.get<boolean>('DB_LOGGING', false),
+        ssl:
+            configService.get<string>('DB_SSL') === 'true'
+                ? {
+                    require: true,
+                    rejectUnauthorized: false,
+                }
+                : false,
     })
 }
